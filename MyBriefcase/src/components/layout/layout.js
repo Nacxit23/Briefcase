a025e43ec2533540c0deb4297d7ff4140e339ad0
Nacxit <tierrasalvaje_02@hotmail.com>
2019-10-11 20:26:18 -0600

    Creating layout component

---
+++ MyBriefcase/src/components/layout/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HeaderBriefcase from "../headerBriefcase/header-briefcase";
+import FooterBriefcase from "../footerBriefcase/footer-briefcase";
+
+export default ({children }) => (
+  <div>
+    <HeaderBriefcase/>
+    {children}
+    <FooterBriefcase/>
+  </div>
+)
