908b99d20230616ccd34e6c0438e424fe8a70e67
Nacxit <tierrasalvaje_02@hotmail.com>
2019-10-15 23:26:47 -0600

    Import and use react icon

--- MyBriefcase/src/components/footerBriefcase/footer-briefcase.js
+++ MyBriefcase/src/components/footerBriefcase/footer-briefcase.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import FlexGrid from "../../pages/globalStyle/flexboxgrid.min.module.css"
 import FooterStyle from "./footer.module.css"
+import { FaFacebookSquare, FaTwitterSquare, FaLinkedin } from "react-icons/fa"
 
 export default () => {
   const data = useStaticQuery(graphql`
@@ -18,22 +19,22 @@ export default () => {
   `)
   return (
     <footer>
-      <div className={`${FlexGrid.col} ${FlexGrid.colXs}`}>
-        <div className={` ${FlexGrid.row} ${FlexGrid.colXs} 
-        ${FlexGrid.aroundXs} ${FlexGrid.middleXs}`}>
+      <div className={`${FlexGrid.col}`}>
+        <div className={` ${FlexGrid.row} ${FlexGrid.aroundXs} 
+        ${FlexGrid.middleXs} ${FlexGrid.colLg} ${FlexGrid.colXs}`}>
           <p className={`${FooterStyle.footerText}`}>
             {data.site.siteMetadata.phoneNumber}
           </p>
-          <div className={` ${FlexGrid.row} ${FlexGrid.aroundXs} ${FlexGrid.middleXs}`}>
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
+          <div className={`${FlexGrid.row} ${FooterStyle.iconBox}`}>
+            <a className={FooterStyle.iconFontAwesome} href=""><FaFacebookSquare/></a>
+            <a className={FooterStyle.iconFontAwesome} href=""><FaTwitterSquare/></a>
+            <a className={FooterStyle.iconFontAwesome} href=""><FaLinkedin/></a>
           </div>
           <p className={`${FooterStyle.footerText}`}>
             {data.site.siteMetadata.personalEmail}
           </p>
         </div>
-        <div className={`${FlexGrid.colXs12} ${FlexGrid.centerXs} ${FooterStyle.copyRight}`}>
+        <div className={`${FlexGrid.centerXs} ${FooterStyle.copyRight}`}>
           <p>
             {data.site.siteMetadata.copyRight}
           </p>
