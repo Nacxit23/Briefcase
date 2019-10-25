fc379acf8b33182d197b8ad53201cdd88dd28bb9
Nacxit <tierrasalvaje_02@hotmail.com>
2019-10-24 18:42:31 -0600

    update layout

--- MyBriefcase/src/components/layout/layout.js
+++ MyBriefcase/src/components/layout/layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import HeaderBriefcase from "../headerBriefcase/header-briefcase";
 import FooterBriefcase from "../footerBriefcase/footer-briefcase";
-
 export default ({children }) => (
   <div>
     <HeaderBriefcase/>
