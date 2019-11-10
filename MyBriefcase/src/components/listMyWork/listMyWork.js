f98c8d0d69d675b31decf16fcde20634f498c931
Nacxit <tierrasalvaje_02@hotmail.com>
2019-11-10 11:46:05 -0600

    creating html and style from My last work component

--- MyBriefcase/src/components/listMyWork/listMyWork.js
+++ MyBriefcase/src/components/listMyWork/listMyWork.js
@@ -1,13 +1,42 @@
-import React from "react";
-import myWorkStyle from "./listMyWork.module.css";
-import flexGrid from "../../pages/globalStyle/flexboxgrid.min.module.css"
+import React from "react"
+import WodSty from "./listMyWork.module.css"
+import flex from "../../pages/globalStyle/flexboxgrid.min.module.css"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-const listMyWork = props => (
-   <div className={`${flexGrid.containerFluid}`}>
-    <div className={`${flexGrid.colMd}`}>
-
+export default () => {
+  const data = useStaticQuery(graphql`
+query {
+ vueProject: file(relativePath: {eq: "workOne.jpg" }) {
+     childImageSharp {
+      fluid(maxWidth:400){
+         ...GatsbyImageSharpFluid
+      }
+    }
+ }
+  laravelProject: file(relativePath: {eq: "autorecue.jpg"}) {
+    childImageSharp {
+      fluid(maxWidth:800) {
+         ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+  `)
+  return (
+    <div className={`${flex.containerFluid}`}>
+      <div className={`${WodSty.circle}`}/>
+      <div className={`${flex.colMd} ${flex.colXs} ${WodSty.conteinerWord}`}>
+        <p>My Latest Work</p>
+        <div className={`${WodSty.lastWork} ${flex.colMd} ${flex.row}`}>
+          <a className={`${flex.hvrPulse}`}>
+            <Img className={`${WodSty.img}`} fluid={data.vueProject.childImageSharp.fluid}/>
+          </a>
+          <a className={`${flex.hvrPulse}`}>
+            <Img className={`${WodSty.img}`} fluid={data.laravelProject.childImageSharp.fluid}/>
+          </a>
+        </div>
+      </div>
     </div>
-   </div>
-)
-
-export default listMyWork;
+  )
+}
