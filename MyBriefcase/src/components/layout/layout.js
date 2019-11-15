265b923082e570e4c177d793d30a992e0b615621
Nacxit <tierrasalvaje_02@hotmail.com>
2019-11-15 12:19:26 -0600

    main children

--- MyBriefcase/src/components/layout/layout.js
+++ MyBriefcase/src/components/layout/layout.js
@@ -1,10 +1,11 @@
 import React from "react"
 import HeaderBriefcase from "../headerBriefcase/header-briefcase"
 import FooterBriefcase from "../footerBriefcase/footer-briefcase"
+
 export default ({ children }) => (
   <div>
-    <HeaderBriefcase />
-    {children}
-    <FooterBriefcase />
+    <HeaderBriefcase/>
+    <main>{children}</main>
+    <FooterBriefcase/>
   </div>
 )
